ff82c2db68e2087e70e6e7021878552d67f10560
Chris Nielsen <cnielsen@arke.com>
2025-03-06 17:53:46 -0500

    Results of testing.

--- cli/build/prepack.js
+++ cli/build/prepack.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 
+import { styleText } from 'node:util';
+import { spawnSync } from 'node:child_process';
 import clean from './lib/clean.js';
 import compileTypeScript from '../../build/lib/compileTypeScript.js';
 import {
@@ -16,9 +18,34 @@ import copyFile from './lib/copyFile.js';
 await clean();
 await preTsc();
 compileTypeScript(tsConfigUrl);
+await postTsc();
+
+runTests();
 
 await Promise.all([
-	postTsc(),
 	copyFile(licenseSrc, licenseDist),
 	copyFile(readmeSrc, readmeDist),
 ]);
+
+function runTests() {
+	const result = spawnSync(
+		'yarn',
+		[
+			'workspace',
+			'@arke-systems/beacon-test',
+			'vitest',
+			'run',
+			'--no-api',
+			'--bail',
+			'1',
+		],
+		{
+			stdio: 'inherit',
+		},
+	).status;
+
+	if (result !== 0) {
+		console.warn(styleText('redBright', 'Tests failed'));
+		process.exit(result);
+	}
+}
