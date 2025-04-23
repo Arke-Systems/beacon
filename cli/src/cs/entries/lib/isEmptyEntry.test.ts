6a4706e2337be4bd877d5d58054dce4012aa76e7
Chris Nielsen <cnielsen@arke.com>
2025-04-23 12:13:25 -0400

    Add a failing unit test.

--- cli/src/cs/entries/lib/isEmptyEntry.test.ts
+++ cli/src/cs/entries/lib/isEmptyEntry.test.ts
@@ -20,42 +20,66 @@ describe(isEmptyEntry.name, () => {
 		{
 			description: 'returns true for empty entries',
 			entry: {
-				ACL: {},
-				_in_progress: true,
-				_version: 1,
-				created_at: '2025-03-03T19:38:14.402Z',
-				created_by: 'another-uid-value',
+				...mockEntry(),
 				display_name: '',
 				link: { href: '', title: '' },
-				locale: 'en-us',
 				logo: {},
 				logo_url: '',
-				tags: [],
-				title: '',
-				uid: 'some-uid-value',
-				updated_at: '2025-03-03T19:38:14.402Z',
-				updated_by: 'another-uid-value',
 			},
 			expected: true,
 		},
 		{
 			description: 'returns false for populated entries',
 			entry: {
-				ACL: {},
-				_in_progress: true,
-				_version: 1,
-				created_at: '2025-03-03T19:38:14.402Z',
-				created_by: 'another-uid-value',
+				...mockEntry(),
 				display_name: 'some-display-name',
 				link: { href: 'some-link-href', title: 'some-link-title' },
-				locale: 'en-us',
 				logo: {},
 				logo_url: 'some-logo-url',
 				tags: ['some-tag'],
 				title: 'some-title',
-				uid: 'some-uid-value',
-				updated_at: '2025-03-03T19:38:14.402Z',
-				updated_by: 'another-uid-value',
+			},
+			expected: false,
+		},
+		{
+			description: 'returns true for entries containing empty RTE fields',
+			entry: {
+				...mockEntry(),
+				description: {
+					type: 'doc',
+					attrs: {},
+					uid: '07cc2e39fcc94427ae255b8e5f32fce2',
+					children: [
+						{
+							type: 'p',
+							attrs: {},
+							uid: '87e76bf2af8d49d2b69a0b63cf83f96a',
+							children: [{ text: '' }],
+						},
+					],
+					_version: 1,
+				},
+			},
+			expected: true,
+		},
+		{
+			description: 'returns false for entries containing populated RTE fields',
+			entry: {
+				...mockEntry(),
+				description: {
+					type: 'doc',
+					attrs: {},
+					uid: '07cc2e39fcc94427ae255b8e5f32fce2',
+					children: [
+						{
+							type: 'p',
+							attrs: {},
+							uid: '87e76bf2af8d49d2b69a0b63cf83f96a',
+							children: [{ text: 'some text goes here' }],
+						},
+					],
+					_version: 1,
+				},
 			},
 			expected: false,
 		},
@@ -78,3 +102,21 @@ describe(isEmptyEntry.name, () => {
 		});
 	});
 });
+
+function mockEntry() {
+	const now = new Date();
+
+	return {
+		ACL: {},
+		_in_progress: true,
+		_version: 1,
+		created_at: now.toISOString(),
+		created_by: 'another-uid-value',
+		locale: 'en-us',
+		tags: [],
+		title: '',
+		uid: 'some-uid-value',
+		updated_at: now.toISOString(),
+		updated_by: 'another-uid-value',
+	};
+}
